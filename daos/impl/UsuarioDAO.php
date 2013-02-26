6209c2464fd9d29bc2a272e3f711d4871ee218f6
Jefferson <Jefferson@Jefferson-PC>
2013-02-25 22:10:03 -0300

    criei metodo delete de usuario

--- daos/impl/UsuarioDAO.php
+++ daos/impl/UsuarioDAO.php
@@ -15,7 +15,7 @@ require_once 'ftbp-src/entidades/basico/Professor.php';
  * @author Luis
  * @since Feb 23, 2013
  */
-class UsuarioDAO extends DAOBasico{
+class UsuarioDAO extends DAOBasico {
 
     /**
      * @param Usuario $entidade
@@ -25,7 +25,7 @@ class UsuarioDAO extends DAOBasico{
         if ($entidade->getTipo() == 'Aluno') {
             $this->inserirAluno($entidade);
         } else {
-            throw new 
+            throw new
             Exception("Não foram implementadas outras formas para gravar usuário");
         }
     }
@@ -34,17 +34,25 @@ class UsuarioDAO extends DAOBasico{
         if ($entidade->getTipo() == 'Aluno') {
             $this->atualizarAluno($entidade);
         } else {
-            throw new 
+            throw new
             Exception("Não foram implementadas outras formas para gravar usuário");
         }
     }
 
     public function executarDelete(Entidade $entidade) {
-        
+
+        $sql = " delete from      
+                      usuarios where id=$1";
+
+        $p = $this->getConn()->prepare($sql);
+
+        $p->setParameter(1, $entidade->getId(), PreparedStatement::INTEGER);
+
+        $p->execute();
     }
 
     private function inserirAluno(Aluno $aluno) {
-        
+
         $sql = "INSERT 
                  INTO usuarios(
                       nome,
@@ -53,45 +61,43 @@ class UsuarioDAO extends DAOBasico{
                       data_criacao,
                       grupo_id)
                VALUES ($1, $2, $3, now(), NULL)";
-        
-        
+
+
         $p = $this->getConn()->prepare($sql);
-        
+
         $p->setParameter(1, $aluno->getNome(), PreparedStatement::STRING);
         $p->setParameter(2, $aluno->getEmail(), PreparedStatement::STRING);
         $p->setParameter(3, hash("sha512", $aluno->getSenha()), PreparedStatement::STRING);
-        
+
         $p->execute();
-        
+
         // Pega o id gerado na sequence 
         $p = $this->getConn()->query("select currval('usuarios_id_seq') as id");
         $p->next();
         $array = $p->fetchArray();
-        
+
         $aluno->setId($array['id']);
-        
-        
     }
+
     private function atualizarAluno(Aluno $aluno) {
-        
+
         $sql = "UPDATE usuarios(
                    SET nome     = $1,
                        email    = $2,
                        senha    = $3,
                        grupo_id = NULL)
                  WHERE id = $4";
-        
+
         $p = $this->getConn()->prepare($sql);
-        
+
         $p->setParameter(1, $aluno->getNome(), PreparedStatement::STRING);
         $p->setParameter(2, $aluno->getEmail(), PreparedStatement::STRING);
         $p->setParameter(3, hash("sha512", $aluno->getSenha()), PreparedStatement::STRING);
         $p->setParameter(4, $aluno->getId(), PreparedStatement::INTEGER);
-        
+
         $p->execute();
-        
     }
-    
+
     /**
      * 
      * @param string $email
@@ -100,30 +106,29 @@ class UsuarioDAO extends DAOBasico{
      * @throws NoResultException
      */
     public function login($email, $senha) {
-        
+
         $sql = "select *  
                  from usuarios
                 where senha = $1 and email = $2";
-        
+
         $p = $this->getConn()->prepare($sql);
         $p->setParameter(1, hash("sha512", $senha), PreparedStatement::STRING);
         $p->setParameter(2, $email, PreparedStatement::STRING);
-        
+
         $rs = $p->getResult();
-        if(!$rs->next()){
+        if (!$rs->next()) {
             throw new NoResultException("Usuário não encontrado");
         }
-        
+
         return $this->montarAluno($rs);
-        
     }
-    
+
     /**
      * 
      * @param ResultSet $rs
      * @return Aluno
      */
-    private function montarAluno(ResultSet $rs){
+    private function montarAluno(ResultSet $rs) {
         $arr = $rs->fetchArray();
         $u = new Aluno();
         $u->setEmail($arr['email']);
@@ -131,7 +136,7 @@ class UsuarioDAO extends DAOBasico{
         $u->setId($arr['id']);
         return $u;
     }
-    
+
     /**
      * @param integer $id
      * @return Aluno
@@ -141,15 +146,15 @@ class UsuarioDAO extends DAOBasico{
         $sql = "select *  
                  from usuarios
                 where id = $1";
-        
+
         $p = $this->getConn()->prepare($sql);
         $p->setParameter(1, $id, PreparedStatement::INTEGER);
         $rs = $p->getResult();
-        
-        if(!$rs->next()){
+
+        if (!$rs->next()) {
             throw new NoResultException("Usuário não encontrado");
         }
-        
+
         return $this->montarAluno($rs);
     }
 
