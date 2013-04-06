df2be3157ffe1c1de9e588c7d86c78ebd95dfb17
felipe <felipe@vmxp.home>
2013-04-06 14:19:31 -0300

    inicio criação método verAviso por usuario

--- daos/impl/AvisoDAO.php
+++ daos/impl/AvisoDAO.php
@@ -2,6 +2,7 @@
 
 require_once 'ftbp-src/daos/EntidadeDAO.php';
 require_once 'ftbp-src/entidades/basico/Aviso.php';
+require_once 'ftbp-src/entidades/basico/Usuario.php';
 require_once 'DAOBasico.php';
 
 
@@ -87,6 +88,8 @@ class AvisoDAO extends DAOBasico {
         $av->setId($arr['id']);
         $av->setTitulo($arr['titulo']);
         $av->setDescricao($arr['descricao']);
+        $av->setCriadoPor(new Usuario());
+        $av->getCriadoPor()->setNome($arr['criadopor']);
         return $av;
     }
 
@@ -113,15 +116,19 @@ class AvisoDAO extends DAOBasico {
          
         // Prepara a querie ordenando pela data decrescente
         
-        $sql = "select av.id as id, av.titulo as titulo, av.descricao as descricao
-                    from aviso av 
-                        join aviso_destinatario ad on av.id = ad.aviso_id
-                        left join usuarios usu on usu.id = ad.usuario_id
+        $sql = "select usu.nome as criadopor, 
+                    avi.titulo as titulo, 
+                    avi.descricao as descricao, 
+                    ad.usuario_id as id_destino,
+                    avi.id as id
+                    from usuarios usu
+                        join aviso avi on usu.id = avi.usuario_id
+                        inner join aviso_destinatario ad on avi.id =  ad.aviso_id
                     where 
                         ad.usuario_id = $1
-                        and av.excluida = false
+                        and avi.excluida = false
                         and ad.ativo = true
-                    order by av.id desc --limit 10";
+                    order by avi.id desc --limit 10";
         
         $p = $this->getConn()->prepare($sql);
         
@@ -141,6 +148,17 @@ class AvisoDAO extends DAOBasico {
         
         // Retorna a lista montada.
         return $list;
+    }
+    
+    public function avisoLido(Entidade $entidade, Usuario $usuario){
+        $sql = "update aviso_destinatario set ativo = false 
+                    where aviso_id=$1
+                        and usuario_id = $2";
+        
+        $p = $this->getConn()->prepare($sql);
+        $p->setParameter(1, $entidade->getId(), PreparedStatement::INTEGER);
+        $p->setParameter(2, $usuario->getId(), PreparedStatement::INTEGER);
+        $p->execute();
     }
 
 }
